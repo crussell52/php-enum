ea7e07159a4ace54516e4c6ed4c0be3545a838be
Chris Russell <crussell52@gmail.com>
2015-08-30 17:26:41 -0400

    Introduced BadEnumNameException
    - Cleaned up documentation in TEnum
    - Invalid Enum names now cause a `BadEnumNameException` to be thrown.

---
+++ src/Exception/BadEnumNameException.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * php-enum
+ *
+ * @author    Chris Russell (crussell52@gmail.com)
+ * @copyright 2014 Chris Russell (https://www.github.com/crussell52/)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://www.github.com/crussell52/php-enum
+ */
+namespace CRusell52\Enum\Exception;
+
+/**
+ * This exception is appropriate for cases when an Enum value is requested by a name which does not match any defined
+ * Enum value.
+ *
+ * @package CRusell52\Enum\Exception
+ */
+class BadEnumNameException extends \RuntimeException
+{
+    /**
+     * The name which failed to resolve to an Enum value.
+     *
+     * @var string
+     */
+    private $_badName;
+
+    /**
+     * A list of available Enum value names.
+     *
+     * @var array
+     */
+    private $_availableNames;
+
+    /**
+     * @param string          $badName         The name which failed to resolve to an Enum value.
+     * @param array           $availableNames  A list of all available Enum value names.
+     * @param string          $message         A custom message for the exception. If omitted, then a default message
+     *                                         which gives the bad name and all available names will be used.
+     * @param int             $code            An optional code which further qualifies the exception case.
+     * @param \Exception|null $previous        The exception which caused this exception.
+     */
+    public function __construct($badName, array $availableNames, $message = "", $code = 0, \Exception $previous = null)
+    {
+        $this->_badName = $badName;
+        $this->_availableNames = $availableNames;
+
+        if ((string)$message === "") {
+            $message = "No Enum value available under that badName ($badName).";
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Provides a list of all available Enum value names.
+     *
+     * @return array
+     */
+    public function getAvailableNames()
+    {
+        return $this->_availableNames;
+    }
+
+    /**
+     * Provides the name which failed to resolve to an Enum name.
+     *
+     * @return string
+     */
+    public function getBadName()
+    {
+        return $this->_badName;
+    }
+}
--- src/TEnum.php
+++ src/TEnum.php
@@ -9,83 +9,65 @@
  */
 namespace CRusell52\Enum;
 
-use ArrayIterator;
-use Iterator;
+use CRusell52\Enum\Exception\BadEnumNameException;
+use Traversable;
 
 /**
- * This trait provides implementation details which can be used to facilitate a PHP implementation
- * of Enumerations similar to that of Java.
+ * This trait provides the functionality necessary for a php-enum implementation.
  *
- * <p>Enumerations (Enums) can be thought of as complex constants with methods attached to them.
+ * <p>Enumerations (Enums) can be thought of a collection of complex constants with methods attached to them.
  * They represent structures which have ONLY constant property values. An example is an Enum which
- * defines available RGB colors. The red, green, and blue value of any given color is always the
- * same.
+ * defines a set of available colors in a chat application. This would be a good candidate for an Enum because:
+ *
+ * <ul>
+ *    <li>The are a predefined and finite set of colors available.
+ *    <li>Each color definition has more than one attribute (i.e. redValue, greenValue, blueValue)
+ *    <li>Every attribute of the color is a constant value. Runtime logic can not affect their values.
+ * </ul>
+ *
  *
  * <p>An Enum also has the opportunity to expose instance methods and/or static methods for
  * operating against an Enum value (instance) or the list of available values.  These methods MUST
  * NOT alter the values of properties within the Enum instance because as all properties MUST be
- * constant values.  Expanding on the Color example from above, an instance method may be provided
- * which returns the HTML code for the Color. A static method may be provided which allows retrieval
+ * constant values.  Expanding on the `ChatColor` example from above, an instance method may be provided
+ * which returns the HTML code for each color. A static method may be provided which allows retrieval
  * of a specific Color given an HTML code.
  *
- * <p>An Enum value is retrieved with the following syntax:
+ *
+ * <p>An Enum value is retrieved by name with the following syntax:
  *
  * <code>
- *    // EnumClass::VALUE_NAME()
- *    Color.RED();
+ *    // EnumClass::VALUE_NAME();
+ *    $color = ChatColor.RED();
  * </code>
  *
- * <p>Because each Enum value is an instance of the Enum implementation, type-hinting can be
- * utilized to ensure that a parameter contains a value represented by that Enum implementation.
+ * <p>Retrieving an Enum value with a non-existent name will throw a BadEnumNameException:
  *
  * <code>
- *   function reportHtmlCode(Color $color)
- *   {
- *      echo $color->getHtmlCode();
- *   }
- *
- *   ...
- *   reportHtmlCode(Color.RED());  // #ff0000
- *
+ *    $color = ChatColor.TABLE(); // throws BadEnumNameException
  * </code>
  *
  * <p>An Enum implementation must follow these rules:
  *
  * <ul>
- *   <li>
- *      All Enum properties MUST contain ONLY constant values. The outside world MAY be able to
- *      access the value of any property but MUST NOT be able to modify the value of any property.
- *   </li>
- *   <li>
- *     Each Enum value must explicitly equal itself, regardless of how it is retrieved. <br/> <br/>
+ *   <li>All Enum properties MUST contain ONLY constant values. The outside world MAY be able to
+ *   access the value of any property but MUST NOT be able to modify the value of any property.
  *
- *     <code>
- *        Color.RED() === Color.RED()
- *        Color.findByHtmlCode("#ff0000") === Color.RED()
- *     <code>
- *   </li>
- *   <li>
- *     All Enum values SHOULD be declared using @method annotation.
- *   </li>
+ *   <li>Each Enum value must equal itself, regardless of how it is retrieved.
  *
- * @link http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+ *   <li>All Enum values SHOULD be declared in the class-level phpDoc using the `method` annotation, where the name
+ *   of the method matches the name of the Enum value.
+ * <ul>
  *
  * @package crussell52/enum
  */
 trait TEnum
 {
   /**
-   * The name of this Enum instance.
+   * An associative array where each key represents an Enum name name in the Enum implementation
+   * and each value is an array of the values which represent the attribute values of the related Enum value.
    *
-   * @var string
-   */
-  private $_name;
-
-  /**
-   * An associative array where each key represents an available name in the Enum implementation
-   * and each value is an array of the values which are used to define the Enum instance.
-   *
-   * <p>The values of this array MUST be populated during execution of _initializeDefinitions().</p>
+   * <p>The values of this array MUST be populated during execution of _initializeDefinitions().
    *
    * @see getName()
    * @see _populate()
@@ -99,22 +81,25 @@ trait TEnum
    * A memory-resident cache of previously created Enum instances, keyed by name.
    *
    * <p>Because an Enum instance represents constant values there is never a need for more than one
-   * instance. Moreover, there CAN NOT be more than one instance because the following assertion must
-   * always be true: </p>
-   *
-   * <code>
-   *  Enum::VALUE() === Enum::VALUE()
-   * </code>
+   * instance.
    *
    * @var array
    */
   private static $_enums = [];
 
   /**
-   * Returns the name of the Enum instance.
+   * The name of the Enum value.
+   *
+   * @var string
+   */
+  private $_name;
+
+  /**
+   * Returns the name of the Enum value.
    *
    * <code>
-   *   echo (string)Color::RED(); // outputs "RED"
+   *   $color = ChatColor::RED();
+   *   echo $color->getName(); // outputs "RED"
    * </code>
    *
    * @return string
@@ -132,7 +117,7 @@ trait TEnum
    *                     defined within self::$_definitions.  It is also possible to retrieve a list
    *                     of available Enum names with the static getNames() method.
    *
-   * @throws \InvalidArgumentException
+   * @throws BadEnumNameException
    */
   private final function __construct($name)
   {
@@ -142,7 +127,7 @@ trait TEnum
     // If the name is not valid, then throw an exception.
     if (!isset(self::$_definitions[$name]))
     {
-      throw new \InvalidArgumentException("Unknown Enum name: $name.");
+      throw new BadEnumNameException($name, self::getNames());
     }
 
     $this->_name = $name;
@@ -150,37 +135,42 @@ trait TEnum
   }
 
   /**
-   * This method initializes the data which defines what Enum names are available and the values
-   * for each.
+   * This method initializes the data which defines what Enum names are available and the values for each.
    *
-   * <p>This method will only be called once for any given Enum subclass. Subclasses should not
-   * need to call this method directly.</p>
+   * <p>Implementations of this method MUST override this method to set the value of self::$_definitions.
    *
-   * <p>Concrete Enum subclasses must override this method to define subclass Enum members. Each key
-   * should represent an Enum name and each value should be an array containing its data. The data
-   * array will be passed into the subclass implementation of _populate().</p>
+   * <p>This method will only be called once for any given Enum implementation. Implementations should never
+   * need to call this method directly.
    *
-   * For example:</p>
+   * <p>Enum implementations MUST set the value of self::$_definitions within this method to define the details of each
+   * Enum value. Each key defines the name of an Enum value and each value MUST be an array defining the Enum value's
+   * attributes in a way which can be interpreted by the implementation's _populate() method.
+   *
+   * <p>For example:
    *
    * <code>
-   *   class Color extends Enum
+   *   class ChatColor
    *   {
-   *     ...
-   *     protected static function _initializeDefinitions()
-   *     {
-   *       self::$_definitions = [
-   *         'RED'    => [255, 0, 0],
-   *         'YELLOW' => [255, 255, 0]
-   *       ];
-   *     }
+   *       use Crussell52\Enum\TEnum;
    *
-   *     ...
-   *     protected function _populate(array $args)
-   *     {
-   *       $this->_red   = $args[0];
-   *       $this->_green = $args[1];
-   *       $this->_blue  = $args[2]
-   *     }
+   *       ...
+   *       protected static function _initializeDefinitions()
+   *       {
+   *           // 'NAME' => [red, green, blue]
+   *           self::$_definitions = [
+   *
+   *               'RED'    => [255, 0, 0],
+   *               'YELLOW' => [255, 255, 0]
+   *           ];
+   *       }
+   *
+   *       ...
+   *       protected function _populate(array $definition)
+   *       {
+   *           $this->_red   = $definition[0];
+   *           $this->_green = $definition[1];
+   *           $this->_blue  = $definition[2];
+   *       }
    *   }
    * </code>
    */
@@ -190,17 +180,29 @@ trait TEnum
   }
 
   /**
-   * {@inheritDoc}
+   * Allows the Enum value to be coerced into a string by returning its name.
+   *
+   * @return string
    */
   public function __toString()
   {
     return $this->_name;
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  public static final function __callStatic($name, $args)
+    /**
+     * Magic Method implementation to provide an Enum value by invoking its name as a class method.
+     *
+     * <code>
+     *     $color = ChatColor.RED(); // __callStatic("RED", []);
+     * </code>
+     *
+     * @param string $name    The name of the Enum value.
+     * @param array  $ignored The magic method receives arguments passed into the original method call, but this
+     *                        implementation ignores them.
+     *
+     * @return static
+     */
+  public static final function __callStatic($name, $ignored)
   {
     $name = (string)$name;
     if (isset(self::$_enums[$name]))
@@ -220,36 +222,39 @@ trait TEnum
   }
 
   /**
-   * This method receives in a single enum definition and populates the instance using that
-   * definition.
+   * This method receives in a single Enum value definition and populates the instance using that definition.
    *
    * <code>
-   *   class Color extends Enum
+   *   class ChatColor
    *   {
-   *     ...
-   *     protected static function _initializeDefinitions()
-   *     {
-   *       self::$_definitions = [
-   *         'RED'    => [255, 0, 0],
-   *         'YELLOW' => [255, 255, 0]
-   *       ];
-   *     }
+   *       use Crussell52\Enum\TEnum;
    *
-   *     ...
-   *     protected function _populate(array $args)
-   *     {
-   *       $this->_red   = $args[0];
-   *       $this->_green = $args[1];
-   *       $this->_blue  = $args[2]
-   *     }
+   *       ...
+   *       protected static function _initializeDefinitions()
+   *       {
+   *           // 'NAME' => [red, green, blue]
+   *           self::$_definitions = [
+   *
+   *               'RED'    => [255, 0, 0],
+   *               'YELLOW' => [255, 255, 0]
+   *           ];
+   *       }
+   *
+   *       ...
+   *       protected function _populate(array $definition)
+   *       {
+   *           $this->_red   = $definition[0];
+   *           $this->_green = $definition[1];
+   *           $this->_blue  = $definition[2];
+   *       }
    *   }
    * </code>
    *
-   * @param array $args
+   * @param array $definition The definition of the enum value being populated.
    *
    * @return void
    */
-  abstract protected function _populate(array $args);
+  abstract protected function _populate(array $definition);
 
   /**
    * This method provides all available names
@@ -270,16 +275,13 @@ trait TEnum
    *
    * <code>
    *    // Output each available color and its html code.
-   *    foreach (Colors.getValues() as $Color)
+   *    foreach (ChatColors.getValues() as $color)
    *    {
-   *       echo $Color.getName() . ": " . $Color.getHTMLCode() . "<br/>";
+   *       echo $color.getName() . ": " . $color.getHTMLCode() . "<br/>";
    *    }
    * </code>
    *
-   * @internal This implementation can be easily updated to use Generators if being executed on
-   *           PHP >= 5.5 for an efficiency gain. The related code is commented out below.
-   *
-   * @return Iterator
+   * @return Traversable
    */
   public final static function getValues()
   {
@@ -290,21 +292,10 @@ trait TEnum
       self::_initializeDefinitions();
     }
 
-//    // PHP 5.5 implementation using Generator.
-//    // Loop over each available definition and return the instance.
-//    foreach (self::$_definitions as $name => $junk)
-//    {
-//      yield static::$name();
-//    }
-
-    // Build out array of available values.
-    $values = [];
+    // Loop over each available definition and return the instance.
     foreach (self::$_definitions as $name => $junk)
     {
-      $values[] = static::$name();
+      yield static::$name();
     }
-
-    // Return a an iterator containing all available values.
-    return new ArrayIterator($values);
   }
 }
